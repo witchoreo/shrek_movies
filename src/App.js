8a19565260040316c80befa64e66725cc50da94f
Victoria <lalacaramelala@gmail.com>
2023-06-03 18:04:57 +0300

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.js 	new file:   src/components/Movie.jsx 	new file:   src/components/Movies.jsx 	new file:   src/components/preloader.jsx 	new file:   src/components/search.jsx 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/layout/footer.jsx 	new file:   src/layout/header.jsx 	new file:   src/layout/main.jsx

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from './layout/header';
+import { Footer } from './layout/footer';
+import { Main } from './layout/main';
+
+
+function App() {
+    return (
+        <React.Fragment>
+            < Header />
+            < Main />
+            < Footer />
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ src/components/preloader.jsx
@@ -0,0 +1,7 @@
+function Preloader() {
+    return <div className="progress green accent-3">
+        <div className="indeterminate light-green accent-4"></div>
+    </div>
+}
+
+export { Preloader };
